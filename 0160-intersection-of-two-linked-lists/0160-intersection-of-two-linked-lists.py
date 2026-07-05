e41f9d120ac611b81dca988369e464b32d6be5de
kaif qureshi <165767122+midnightshady@users.noreply.github.com>
2026-07-05 23:46:54 +0530

    Time: 285 ms (8.87%), Space: 67 MB (19.45%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -4,52 +4,41 @@
 #         self.val = x
 #         self.next = None
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        lengthA = 0
-        lengthB = 0
-        currentA = headA
-        currentB = headB
+
+        def getLength(head):
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+
+        lengthA = getLength(headA)
+        lengthB = getLength(headB)
+
         pA = headA
         pB = headB
 
-        while currentA:
-            lengthA += 1
-            currentA = currentA.next
-        while currentB:
-            lengthB += 1
-            currentB = currentB.next
-
         if lengthA > lengthB:
-            diff = lengthA - lengthB
-            while diff:
+            for _ in range(lengthA - lengthB):
                 pA = pA.next
-                diff -= 1
-            while pA or pB:
-                if pA == pB:
-                    return pA
-                pA = pA.next
+        else:
+            for _ in range(lengthB - lengthA):
                 pB = pB.next
 
-        if lengthB > lengthA:
-            diff = lengthB - lengthA
-            while diff:
-                pB = pB.next
-                diff -= 1
-            while pA or pB:
-                if pA == pB:
-                    return pB
-                pA = pA.next
-                pB = pB.next
-        
-        while pA or pB:
+        while pA and pB:
             if pA == pB:
-                return pB
+                return pA
             pA = pA.next
             pB = pB.next
+
+        return None
 
                            
