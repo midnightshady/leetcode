46236dc3c448c86f2c0ee29aabcd08d8dd789243
kaif qureshi <165767122+midnightshady@users.noreply.github.com>
2026-07-10 12:19:56 +0530

    Time: 143 ms (55.52%), Space: 15.4 MB (59.23%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -11,14 +11,12 @@ class Solution(object):
         for right in range(len(s)):
             freq[s[right]] = freq.get(s[right], 0) + 1
             mx = max(mx, freq[s[right]])
-            win = right - left + 1
-            while win - mx > k:
+            while (right - left + 1) - mx > k:
                 f = s[left]
                 freq[f] -= 1
                 left += 1
                 if freq[f] == 0:
                     del freq[f]
-                win = right - left + 1
             total = right - left + 1
             res = max(res, total)
         return res
