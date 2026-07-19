0135eed8a787ae8c00758076f02ffc8f2541c1c3
midnight.shady <kaifqureshi3672@gmail.com>
2026-07-19 14:54:05 +0530

    Time: 19 ms (97.42%), Space: 13.4 MB (24.1%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -5,46 +5,34 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
-        if not s or not words:
-            return []
-
-        word_length = len(words[0])
-        total_length = len(words) * word_length
         result = []
-        word_count = {}
+        
+        words_count = {}
+        word_length = len(words[0])
+        total_length = word_length * len(words)
 
         for word in words:
-            if word in word_count:
-                word_count[word] += 1
-            else:
-                word_count[word] = 1
-        
+            words_count[word] = words_count.get(word, 0) + 1
+
         for i in range(word_length):
             left = i
-            sub_count = {}
             count = 0
+            sub_count = {}
 
             for j in range(i, len(s) - word_length + 1, word_length):
-                sub_word = s[j:j + word_length]
-
-                if sub_word in word_count:
-                    if sub_word in sub_count:
-                        sub_count[sub_word] += 1
-                    else:
-                        sub_count[sub_word] = 1
+                sub_word = s[j : j + word_length]
+                if sub_word in words_count:
+                    sub_count[sub_word] = sub_count.get(sub_word, 0) + 1
                     count += 1
-
-                    while sub_count[sub_word] > word_count[sub_word]:
+                    while sub_count[sub_word] > words_count[sub_word]:
                         sub_count[s[left:left + word_length]] -= 1
                         count -= 1
-                        left += word_length
-
+                        left +=  word_length
                     if count == len(words):
                         result.append(left)
-
                 else:
                     sub_count.clear()
-                    count = 0 
+                    count = 0
                     left = j + word_length
-
         return result
+                    
