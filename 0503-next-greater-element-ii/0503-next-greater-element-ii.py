b4ce8df86ea1ddbf2beb028b519cf8604552f568
midnight.shady <kaifqureshi3672@gmail.com>
2026-07-25 12:52:27 +0530

    Time: 49 ms (25.15%), Space: 14.3 MB (30.76%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -5,18 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        ans = [-1] * n
         stack = []
+        ans = [-1] * n
 
-        for i in range(2 * n - 1, -1, -1):
-
+        for i in range(2 * n - 1, - 1, - 1):
             while stack and stack[-1] <= nums[i % n]:
                 stack.pop()
-
-            if i < n:
+            if i < n :
                 if stack:
                     ans[i] = stack[-1]
-
             stack.append(nums[i % n])
 
         return ans
+        
