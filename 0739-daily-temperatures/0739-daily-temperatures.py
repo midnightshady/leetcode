bab8a41fd091b175912fa18892fed4bf9415c5f5
midnight.shady <kaifqureshi3672@gmail.com>
2026-07-25 14:23:36 +0530

    Time: 204 ms (17.96%), Space: 27.2 MB (13.45%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []
+
+        for i in range(n - 1, - 1, - 1):
+            while stack and temperatures[stack[-1]] <=  temperatures[i]:
+                stack.pop()
+            if stack :
+                ans[i] = stack[-1] - i
+            stack.append(i)
+        return ans
