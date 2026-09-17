43871535822f37a5106d2f0e38fb6f0c29fd471e
midnight.shady <kaifqureshi3672@gmail.com>
2026-09-17 11:58:12 +0530

    Time: 0 ms (100%), Space: 24.6 MB (67.77%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def hIndex(self, citations: list[int]) -> int:
-        count = [0] * (max(citations) + 1)
-        i = 0
-        while(i < len(citations)):
-            count[citations[i]] += 1
-            i += 1
-        
-        i = 0
-        for num in range(len(count)):
-            while count[num] > 0:
-                citations[i] = num
-                i += 1
-                count[num] -= 1
         n = len(citations)
-        for i in range(len(citations)):
-            if citations[i] >= n - i:
-                return n - i
-        return 0
+        start = 0
+        end = n - 1
+        ans = 0
+        
+        while start <= end:
+            mid = start + (end - start) // 2
+            if citations[mid] >= n - mid:
+                ans = n - mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        return ans
